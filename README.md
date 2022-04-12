e72b0b64e9c842c4e76f0c4fa25d9c9505a50582
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2022-04-12 19:19:40 +0300

    mini fixes

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - [Verified contract on etherscan](https://rinkeby.etherscan.io/address/0xac0186d44846f154DaC7b6141F7ACe92baAE32e4)
 - [Verified staking contract](https://rinkeby.etherscan.io/address/0x954F24a62A1986A3F45c54C8E2383b6B1f13d1Fd)
 - [Add Liquidity](https://app.uniswap.org/#/add/v2/ETH/0xac0186d44846f154DaC7b6141F7ACe92baAE32e4?chain=rinkeby)
+- [NFT](https://testnets.opensea.io/assets/0xfc7d047cf9d5a0b3af1ed6b13382ccdb4c22dbee/1)
+- [NFT Contract](https://rinkeby.etherscan.io/address/0xFc7d047CF9D5a0B3aF1Ed6b13382CcDB4C22Dbee)
--- tasks/mintNft.ts
+++ tasks/mintNft.ts
@@ -3,13 +3,18 @@ const fs = require("fs");
 const IPFSClient = require("ipfs-http-client");
 const path = require("path");
 
-const addFile = async (fileName: string, filePath: string) => {
+const addFile = async (
+  fileName: string,
+  filePath: string,
+  describe?: string
+) => {
   const ipfs = await IPFSClient.create({
     host: "ipfs.infura.io",
     port: 5001,
     protocol: "https",
   });
   const file = fs.readFileSync(path.resolve(__dirname, filePath));
+
   const filesAdded = await ipfs.add(
     { path: fileName, content: file },
     {
@@ -22,12 +27,18 @@ const addFile = async (fileName: string, filePath: string) => {
   const jsonAddes = await ipfs.add(
     JSON.stringify({
       name: fileName,
-      description: "my describe",
+      description: describe || "no description",
       image: `https://ipfs.infura.io/ipfs/${fileHash}`,
       attributes: [],
     })
   );
-  console.log(jsonAddes);
+
+  console.log("Metadata: ", {
+    name: fileName,
+    description: describe || "no description",
+    image: `https://ipfs.infura.io/ipfs/${fileHash}`,
+    attributes: [],
+  });
 
   return jsonAddes.cid.toString();
 };
@@ -37,26 +48,28 @@ task("mint-nft")
   .addParam("contractaddr")
   .addParam("path")
   .addParam("name")
+  .addOptionalParam("describe")
   .setAction(
     async ({
       owner,
       contractaddr,
       path,
       name,
+      describe,
     }: {
       owner: string;
       contractaddr: string;
       path: string;
       name: string;
+      describe?: string;
     }) => {
       const NFT = await ethers.getContractFactory("GoldenTiket");
       const nft = await NFT.attach(contractaddr);
-      const tokenHash = await addFile(name, path);
+      const tokenHash = await addFile(name, path, describe);
       await nft.mintToken(owner, tokenHash);
       console.log(
         `Token was minted at address: https://ipfs.infura.io/ipfs/${tokenHash}`
       );
-      console.log(await nft.tokenURI(0), await nft.ownerOf(0));
     }
   );
 // https://ipfs.infura.io/ipfs/QmeH8gy2tKBAxrn5k76Je4sAPAnviZAP6fiU2kwMs5nQnR
